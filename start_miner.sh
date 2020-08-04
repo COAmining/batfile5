a9ff2c83bc041590c0c43db2d2b9d1e0b20ea517
COAmining <68738548+COAmining@users.noreply.github.com>
2020-08-05 02:16:45 +0900

    Add files via upload

---
+++ start_miner.sh
@@ -0,0 +1,1 @@
+/home/coa/cob/PhoenixMiner_5.0e_Linux/PhoenixMiner -pool 159.89.197.105:33456 -wal 0x8CfF35F3F7b4A8F502820e132C60fc1F0aE38e4f.0700 -pool2 ssl://asia1.ethermine.org:5555 -pass zil1ncps6a2t65wdj9az9tektsxsh4ncqqe3qqmtcg@ssl://asia1.ethermine.org:5555 -worker 0700 -proto 2 -log 0 -ptimeout 60 -amd
